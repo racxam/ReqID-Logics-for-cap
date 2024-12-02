5f09094c8f3dc6540856cdff9d9de1bd1b4ee3fc
Sumit Kumar Jhaldiyal <sumitaqurish@gmail.com>
2024-12-02 10:07:51 +0000

    added the complete logic

--- srv/service.js
+++ srv/service.js
@@ -1,23 +1,43 @@
 
-module.exports= async function(){
-    const { OfficeSrv, SeqTrackerSrv }= this.entities;
-    this.before('CREATE', OfficeSrv,(req,res)=>{
+module.exports = async function () {
+    const { OfficeSrv, SeqTrackerSrv } = this.entities;
+    this.before('CREATE', OfficeSrv, (req, res) => {
         console.log("Hello i am good");
-        
+
     })
-    this.on('CREATE', OfficeSrv,officeReqCreate);
+    this.on('CREATE', OfficeSrv, officeReqCreate);
 
 
-    async function officeReqCreate(req,res) {
+    async function officeReqCreate(req, res) {
         const db = cds.tx(req);
-        const result= await db.run(
+        const result = await db.run(
             SELECT.one('MAX(lastSeq) AS lastSeq').from(SeqTrackerSrv)
         )
-        const lastSeq= result.lastSeq;
-        let BASE_REQID=R
+        const lastSeq = result.lastSeq;
+        const ReqID = generateReqID(lastSeq);
+        console.log("New ReqID is :"+ReqID);
 
-        console.log(result);
-        return result;
+        
+
+        return {ReqID:ReqID};
     }
 
+};
+function generateReqID(lastSeq) {
+    let base = "R"
+    let maxlen = 8;
+
+        let zeros = "0".repeat(maxlen - noOfDigit(lastSeq));
+        let ReqID = base + zeros + (lastSeq+1);
+        console.log(ReqID);
+
+        return ReqID;
 }
+
+function noOfDigit(n) {
+    return Math.floor(Math.log10(n)) + 1;
+}
+
+
+
+
